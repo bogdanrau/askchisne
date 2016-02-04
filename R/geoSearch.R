0b41b472d82bc64fe8cb591a1214b2f7aeb36e04
Bogdan Rau <bogdan.rau@gmail.com>
2016-02-04 10:34:39 -0800

    created initial documentation

--- R/geoSearch.R
+++ R/geoSearch.R
@@ -1,3 +1,14 @@
+#' Search locations by name
+#' Use this function to search for locations within the AskCHIS NE database
+#' by name and obtain geoId, and location type.
+#' @param search Search term (required).
+#' @param apiKey Your API key (required).
+#' @keywords askchis chis
+#' @export
+#' @examples 
+#' geoSearch(search = 'Los Angeles', apiKey = <YOUR API KEY>)
+#' Returns all locations that match 'Los Angeles'
+#' 
 geoSearch <- function(search, apiKey) {
   url <- "http://askchisne.azure-api.net/api/geosearch"
   data <- data.frame(t(sapply(content(GET(url, query = list(searchTerm = search, key = apiKey)), as = "parsed"), c)))
